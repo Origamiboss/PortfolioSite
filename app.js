210a378385563adb6927b9e8fd9abc9df3baf9e7
Origamiboss <vincentclimer@gmail.com>
2026-05-01 19:59:01 -0500

    Removed the needless old pages and renovated the home/projects page

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/mainPage');
-var portfolioRouter = require('./routes/portfolioMainPage');
 var aboutRouter = require('./routes/about');
 var contactRouter = require('./routes/contact');
 var projectRouter = require('./routes/projects');
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/portfolio', portfolioRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
 app.use('/projects', projectRouter);
