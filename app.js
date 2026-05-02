4fccb83ab441196de44d1305279c728227dd01b4
Origamiboss <vincentclimer@gmail.com>
2026-05-02 17:04:33 -0500

    Added the beginnings of the game

--- app.js
+++ app.js
@@ -9,9 +9,47 @@ var aboutRouter = require('./routes/about');
 var contactRouter = require('./routes/contact');
 var projectRouter = require('./routes/projects');
 var usersRouter = require('./routes/users');
+var supremeVillainRouter = require('./routes/supremeVillain');
 
 var app = express();
 
+
+
+//Handle middle ware file setup
+// Only rewrite files that are actually compressed
+app.get('*.wasm', (req, res, next) => {
+    if (!req.url.endsWith('.br') && req.headers['accept-encoding']?.includes('br')) {
+        req.url += '.br';
+        res.set('Content-Encoding', 'br');
+        res.set('Content-Type', 'application/wasm');
+    }
+    next();
+});
+
+app.get('*.data', (req, res, next) => {
+    if (!req.url.endsWith('.br') && req.headers['accept-encoding']?.includes('br')) {
+        req.url += '.br';
+        res.set('Content-Encoding', 'br');
+        res.set('Content-Type', 'application/octet-stream');
+    }
+    next();
+});
+app.use((req, res, next) => {
+    if (req.url.endsWith('.br')) {
+        res.set('Content-Encoding', 'br');
+
+        if (req.url.endsWith('.js.br')) {
+            res.set('Content-Type', 'application/javascript');
+        } else if (req.url.endsWith('.wasm.br')) {
+            res.set('Content-Type', 'application/wasm');
+        } else if (req.url.endsWith('.data.br')) {
+            res.set('Content-Type', 'application/octet-stream');
+        }
+    }
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -20,13 +58,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
 app.use('/projects', projectRouter);
 app.use('/users', usersRouter);
+app.use('/supremeVillainDemo', supremeVillainRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
